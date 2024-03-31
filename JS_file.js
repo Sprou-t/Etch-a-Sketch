3999b053344e1fc91a35b78d42e9390592803446
Sprou-t <150780574+Sprou-t@users.noreply.github.com>
2024-03-31 22:18:19 +0800

    Completed the grid layout using for loop and flexbox

--- JS_file.js
+++ JS_file.js
@@ -1,4 +1,36 @@
 //create 16x16 divs using nested for loops
+    //get reference
+let container = document.querySelector('#container');
+
+function grid_generator(){
+    //for loop
+    for(let i=0; i<16 ; i++){
+        //create rows: need this step as we r not using array
+        const row = document.createElement('div');
+        row.classList.add('row');
 
 
-//append the entire grid to .container div
+        for(let j=0; j <16; j++)
+        {
+            //create divs
+            const cell = document.createElement('div');
+           
+            //add class 
+            cell.classList.add('cell');
+
+            //append cell to row
+            row.appendChild(cell);
+        }
+
+        //append the row to container at the end after each row iteration
+        container.appendChild(row);
+    }
+}
+
+function hover(){
+//set up a event listener when mouse passes over the grid using delegation
+    //create event for mouse to hover over
+    let hover = new Event()
+}
+
+grid_generator();
